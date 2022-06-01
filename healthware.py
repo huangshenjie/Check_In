094e566d22241a37793cbac2105af53990b729f9
huangshenjie <104266571+huangshenjie@users.noreply.github.com>
2022-06-02 00:22:25 +0800

    Update healthware.py

--- healthware.py
+++ healthware.py
@@ -8,87 +8,39 @@ password = sys.argv[2]
 def run(playwright: Playwright) -> None:
     browser = playwright.firefox.launch(headless=False)
     context = browser.new_context()
-
-    # Open new page
     page = context.new_page()
-
-    # Go to https://my.zcst.edu.cn/_web/sopplus/sopplus/index.html
-    page.goto("https://my.zcst.edu.cn/_web/sopplus/sopplus/index.html")
-
-    # Go to https://authserver.zcst.edu.cn/cas/login?service=https%3A%2F%2Fmy.zcst.edu.cn%2FportalRedirect.jsp%3F_p%3DYXM9MSZwPTEmbT1OJg__
-    page.goto("https://authserver.zcst.edu.cn/cas/login?service=https%3A%2F%2Fmy.zcst.edu.cn%2FportalRedirect.jsp%3F_p%3DYXM9MSZwPTEmbT1OJg__")
-
-    # Click [placeholder="用户名\/手机号\/邮箱"]
+    page.goto("https://my.zcst.edu.cn/_web/sopplus/sopplus/index.html", timeout = 0)
     page.locator("[placeholder=\"用户名\\/手机号\\/邮箱\"]").click()
-
-    # Fill [placeholder="用户名\/手机号\/邮箱"]
     page.locator("[placeholder=\"用户名\\/手机号\\/邮箱\"]").fill(username)
-
-    # Click [placeholder="密码"]
     page.locator("[placeholder=\"密码\"]").click()
-
-    # Press CapsLock
     page.locator("[placeholder=\"密码\"]").press("CapsLock")
-
-    # Fill [placeholder="密码"]
     page.locator("[placeholder=\"密码\"]").fill("D")
-
-    # Press CapsLock
     page.locator("[placeholder=\"密码\"]").press("CapsLock")
-
-    # Fill [placeholder="密码"]
     page.locator("[placeholder=\"密码\"]").fill(password)
-
-    # Click text=5天内自动登录 登 录 >> input[name="submit"]
     page.locator("text=5天内自动登录 登 录 >> input[name=\"submit\"]").click()
-    # expect(page).to_have_url("https://my.zcst.edu.cn/_web/sopplus/sopplus/index.html")
-
-    # Click text=查看更多+
-    with page.expect_popup() as popup_info:
-        page.locator("text=查看更多+").click()
-    page1 = popup_info.value
-    
-    # Click text=健康卡填报
-    with page1.expect_popup() as popup_info:
-        page1.locator("text=健康卡填报").click()
-    page2 = popup_info.value    
+    with context.expect_page() as page_info:
+        page.click("text=查看更多+"),
+    page1 = page_info.value
+    with context.expect_page() as page_info:
+        page1.click("text=健康卡填报"),
+    page2 = page_info.value 
     time.sleep(5)
-    # Click a:has-text("我要办理")
-    with page2.expect_popup() as popup_info:
-        page2.locator("a:has-text(\"我要办理\")").click()
-    page3 = popup_info.value
+    with context.expect_page() as page_info:
+        page2.click("a:has-text(\"我要办理\")"),
+    page3 = page_info.value
     time.sleep(5)
-    page3.click("text=为了全力做好学校新型冠状病毒感染的肺炎疫情防控工作，我承诺以下内容填写属实。 30s 已阅读并同意 >> ins")
-    page3.click("text=下一步")
-
-    # Click #select2-select_pcode-container
+    page3.locator("text=为了全力做好学校新型冠状病毒感染的肺炎疫情防控工作，我承诺以下内容填写属实。 30s 已阅读并同意 >> ins").click()
+    page3.locator("text=下一步").click()
     page3.locator("#select2-select_pcode-container").click()
-
-    # Click li[role="treeitem"]:has-text("广东省")
     page3.locator("li[role=\"treeitem\"]:has-text(\"广东省\")").click()
-
-    # Click #select2-select_ccode-container
     page3.locator("#select2-select_ccode-container").click()
-
-    # Click li[role="treeitem"]:has-text("珠海市")
     page3.locator("li[role=\"treeitem\"]:has-text(\"珠海市\")").click()
-
-    # Click #select2-select_dcode-container
     page3.locator("#select2-select_dcode-container").click()
-
-    # Click li[role="treeitem"]:has-text("金湾区")
     page3.locator("li[role=\"treeitem\"]:has-text(\"金湾区\")").click()
-
-    # Click #hsjcsj i
     page3.locator("#hsjcsj i").click()
-
-    # Click text=确定 >> nth=1
     page3.locator("text=确定").nth(1).click()
-    page3.click("text=本人承诺登记后、到校前不再前往其他地区 >> ins")
-    page3.click("button:has-text(\"提交\")")
-    page3.click("a:has-text(\"确定\")")
-
-    # Close page
+    page3.locator("text=本人承诺登记后、到校前不再前往其他地区 >> ins").click()
+    page3.locator("button:has-text(\"提交\")").click()
     page3.close()
 
     # ---------------------
